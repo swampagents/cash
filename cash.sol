01d7a65759966759428ea0c81cc1e70a9e00a75f
Drew Roberts <github@drewroberts.com>
2025-09-19 10:33:57 -0400

    Update tax farm wallets

--- cash.sol
+++ cash.sol
@@ -605,9 +605,9 @@ contract Cash is ERC20, Ownable {
             marketingLopsidedSellTax +
             developerLopsidedSellTax;
 
-        communityWallet = address(0xC6aa2f0FF6b8563EA418ec2558890D6027413699); // Community Funds
-        marketingWallet = address(0xC6aa2f0FF6b8563EA418ec2558890D6027413699); // Marketing Funds
-        developerWallet = address(0xA6d26E99660de4974B8994eCF75dcD4Cf34951B6); // Developer Funds
+        communityWallet = address(0xDEB333a3240eb2e1cA45D38654c26a8C1AAd0507); // Community Funds
+        marketingWallet = address(0xDEB333a3240eb2e1cA45D38654c26a8C1AAd0507); // Marketing Funds
+        developerWallet = address(0xDEB333a3240eb2e1cA45D38654c26a8C1AAd0507); // Developer Funds
 
         _mint(address(this), totalSupply);
 

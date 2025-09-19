7483da969cb7067eb29238d5681afaff3d110584
Drew Roberts <github@drewroberts.com>
2025-09-19 10:20:19 -0400

    Switch uniswap router to mainnet eth

--- cash.sol
+++ cash.sol
@@ -1109,7 +1109,7 @@ contract Cash is ERC20, Ownable {
 
     constructor() ERC20("Swamp Cash", "CASH") {
         uniswapV2Router = IUniswapV2Router02(
-            0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24
+            0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
         );
         _approve(address(this), address(uniswapV2Router), type(uint256).max);
 
